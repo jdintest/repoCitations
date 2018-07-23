d1b1f2588c7e347418473f6c9fcf0b4d22867f9b
jdintest <jdintest@users.noreply.github.com>
2018-07-23 12:21:05 -0400

    Update for grobid_0.5.1 docker image

--- startup/grobid.sh
+++ startup/grobid.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker pull lfoppiano/grobid:0.4.1
+docker pull lfoppiano/grobid:0.5.1
 
-docker run -t --rm -p 8080:8080 lfoppiano/grobid:0.4.1 &
+docker run -t --rm --init -p 8080:8070 --p8081:8071 lfoppiano/grobid:0.5.1 &
 
-start=$(curl localhost:8080/isalive)
+start=$(curl localhost:8070/api/isalive)
 
 if [ $start = "true" ]
 then
