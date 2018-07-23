bd73afbf8c3cb129b43300d448392796410af2d6
jdintest <jdintest@users.noreply.github.com>
2018-07-23 13:16:47 -0400

    Update grobid.sh

--- startup/grobid.sh
+++ startup/grobid.sh
@@ -2,7 +2,7 @@
 
 docker pull lfoppiano/grobid:0.5.1
 
-docker run -t --rm --init -p 8080:8070 --p8081:8071 lfoppiano/grobid:0.5.1 &
+docker run -t --rm --init -p 8080:8070 -p 8081:8071 lfoppiano/grobid:0.5.1 &
 
 start=$(curl localhost:8070/api/isalive)
 
